eb28daca474020cef955fc8a57f0d96ad8910388
Damien Bachet <dbachet@gmail.com>
2015-04-22 23:35:20 +0200

    fix wrong setup for Stripe api key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,4 @@ Rails.configuration.stripe = {
   :secret_key => ENV['STRIPE_SECRET_KEY']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:stripe_secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
